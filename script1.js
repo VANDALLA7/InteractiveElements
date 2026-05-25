c6b49e2d6f90c179029da17291b519d5a1d84b61
VANDALLA <nk0211nastya@gmail.com>
2026-05-25 10:33:34 +0500

    Add files via upload

--- script1.js
+++ script1.js
@@ -34,3 +34,24 @@ window.addEventListener('DOMContentLoaded', () => {
         firstAnswer.style.maxHeight = firstAnswer.scrollHeight + 'px';
     }
 });
+
+
+const tabLinks = document.querySelectorAll('.tablinks');
+const tabContents = document.querySelectorAll('.tabcontent');
+
+
+function openTab(evt) {
+    
+    const btnTarget = evt.currentTarget;
+    const day = btnTarget.getAttribute('data-day');
+
+    tabContents.forEach(content => content.classList.remove('tabcontent-active'));
+    tabLinks.forEach(link => link.classList.remove('tablinks-active'));
+
+    document.querySelector(`#${day}`).classList.add('tabcontent-active');
+    btnTarget.classList.add('tablinks-active');
+}
+
+tabLinks.forEach(link => {
+    link.addEventListener('click', openTab);
+});
